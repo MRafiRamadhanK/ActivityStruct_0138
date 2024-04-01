9f18fa44c4cf8b6fb24d1e90d6d6cef97947baec
MRafiramadhanK <raficool271003@gmail.com>
2024-04-01 15:12:35 +0700

    cout alamat dan umur code 3

--- Activityscruptpt.4_0138/Activityscruptpt.4_0138.cpp
+++ Activityscruptpt.4_0138/Activityscruptpt.4_0138.cpp
@@ -14,9 +14,9 @@ struct mahasiswa {
 int main() {
 	mahasiswa mhs;
 	cout << "Nomor mahasiswa : ";
-	getline (cin, mhs.nim);
+	getline(cin, mhs.nim);
 	cout << "Nama Mahasiswa : ";
-	getline (cin, mhs.nama);
+	getline(cin, mhs.nama);
 
 	cout << "alamat mahasiswa : " << endl;
 	cout << "\t nama Desa : ";
@@ -26,11 +26,11 @@ int main() {
 	cout << "Umur mahasiswa : ";
 	cin >> mhs.umur;
 
-cout << endl;
-cout << "\n Nim :" << mhs.nim;
-cout << "\n Nama : " << mhs.nama;
-cout << "\n alamat : ";
-cout << "\n \t Desa : " << mhs.alamat.desa;
-cout << "\n \t Kota : " << mhs.alamat.kota;
-cout << "\n Umur : " << mhs.umur;
+	cout << endl;
+	cout << "\n Nim :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n alamat : ";
+	cout << "\n \t Desa : " << mhs.alamat.desa;
+	cout << "\n \t Kota : " << mhs.alamat.kota;
+	cout << "\n Umur : " << mhs.umur;
 }
