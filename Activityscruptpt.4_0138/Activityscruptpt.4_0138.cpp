6a3862d119a8a7bdf21d61a070e6f9aadbc5e15d
MRafiramadhanK <raficool271003@gmail.com>
2024-04-01 15:24:32 +0700

    menambahkan jumlah mahasiswa pada int main

--- Activityscruptpt.4_0138/Activityscruptpt.4_0138.cpp
+++ Activityscruptpt.4_0138/Activityscruptpt.4_0138.cpp
@@ -15,23 +15,25 @@ int main() {
 	mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
 		cout << "Nomor mahasiswa : ";
-		getline(cin, mhs.nim);
+		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa : ";
-		getline(cin, mhs.nama);
+		getline(cin, mhs[i].nama);
 
 		cout << "alamat mahasiswa : " << endl;
 		cout << "\t nama Desa : ";
-		cin >> mhs.alamat.desa;
+		cin >> mhs[i].alamat.desa;
 		cout << "\t Nama kota : ";
-		cin >> mhs.alamat.kota;
+		cin >> mhs[i].alamat.kota;
 		cout << "Umur mahasiswa : ";
-		cin >> mhs.umur;
+		cin >> mhs[i].umur;
 	}
-	cout << endl;
-	cout << "\n Nim :" << mhs.nim;
-	cout << "\n Nama : " << mhs.nama;
-	cout << "\n alamat : ";
-	cout << "\n \t Desa : " << mhs.alamat.desa;
-	cout << "\n \t Kota : " << mhs.alamat.kota;
-	cout << "\n Umur : " << mhs.umur;
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "\n Nim :" << mhs.nim;
+		cout << "\n Nama : " << mhs.nama;
+		cout << "\n alamat : ";
+		cout << "\n \t Desa : " << mhs.alamat.desa;
+		cout << "\n \t Kota : " << mhs.alamat.kota;
+		cout << "\n Umur : " << mhs.umur;
+	}
 }
