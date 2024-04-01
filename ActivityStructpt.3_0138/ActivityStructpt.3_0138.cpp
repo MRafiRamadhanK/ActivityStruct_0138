4ecb8f98e1fb388bc65069f1a59faaef8fd2822b
MRafiramadhanK <raficool271003@gmail.com>
2024-04-01 14:40:35 +0700

    int main nama mahasiswa

--- ActivityStructpt.3_0138/ActivityStructpt.3_0138.cpp
+++ ActivityStructpt.3_0138/ActivityStructpt.3_0138.cpp
@@ -11,3 +11,12 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
+
+}
