c94b80d0148719fc12f59fbcd99bb075df444f15
MRafiramadhanK <raficool271003@gmail.com>
2024-04-01 14:16:19 +0700

    struct mahasiswa pt.2

--- ActivityStruct_0138/ActivityStruct_0138.cpp
+++ ActivityStruct_0138/ActivityStruct_0138.cpp
@@ -5,7 +5,7 @@ struct mahasiswa {
 	string nim;
 	string nama;
 	string alamat;
-	string umur;
+	int umur;
 };
 
 int main(){
---
+++ ActivityStructpt.2_0138/ActivityStructpt.2_0138.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string
+};
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
