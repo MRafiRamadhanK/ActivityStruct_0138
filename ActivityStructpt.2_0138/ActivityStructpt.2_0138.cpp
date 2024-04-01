298451274f88132177c853152f2bbefdbe6b93f4
MRafiramadhanK <raficool271003@gmail.com>
2024-04-01 14:24:19 +0700

    int main code 2

--- ActivityStructpt.2_0138/ActivityStructpt.2_0138.cpp
+++ ActivityStructpt.2_0138/ActivityStructpt.2_0138.cpp
@@ -11,7 +11,29 @@ struct DetailAlamat {
 struct mahasiswa {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	int umur;
 };
 
+int main() {
+	mahasiswa  mhs;
+	cout << "nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa :";
+	cin >> mhs.nama;
+
+	cout << "alamat mahasiswa : ";
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.Desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+
+	cout << "Umur mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+}
